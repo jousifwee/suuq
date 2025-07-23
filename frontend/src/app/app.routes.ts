246e2ce5da03f8138c7ed7054e4c91685a0429de
Nic <Nico.Wulff@gmx.net>
2025-07-23 13:41:28 +0200

    feat: add AngebotSeiteStep1 component with data fetching and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { AngebotSeite } from './components/angebot-seite.component/angebot-seite.component';
-
+import { AngebotSeiteStep1 } from './components/angebot-seite-steps/angebot-seite-step1';
 
 export const routes: Routes = [
-    { path: '', component: AngebotSeite }
+    { path: '', component: AngebotSeite },
+    { path: 'step1', component: AngebotSeiteStep1}
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet} from '@angular/router';
 import { AngebotSeite } from "./components/angebot-seite.component/angebot-seite.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
